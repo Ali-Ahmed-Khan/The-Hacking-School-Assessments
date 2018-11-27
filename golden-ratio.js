cb8a33c682dddf4aa0f09ed2fd0e69191f3a46bf
Ali-Ahmed-Khan <IIaliahmedkhanII@gmail.com>
2018-11-27 16:58:10 +0530

    Golden ratio with DOM and HTML

---
+++ golden-ratio.js
@@ -0,0 +1,73 @@
+var div1 = document.createElement("div");
+      div1.id = "a1";
+        div1.style.backgroundColor = "#1a1a1d";
+          div1.style.width="2459.43px";
+            div1.style.height="1520px";
+    
+    
+var bd = document.getElementById("body");
+              bd.appendChild(div1);
+    
+var div3 = document.createElement("div");
+      div3.id = "c3";
+        div3.style.backgroundColor = "#4e4e50";
+          div3.style.width="939.43px";
+            div3.style.height="1520px";
+
+var bd = document.getElementById("a1");
+      bd.appendChild(div3);
+
+var div4 = document.createElement("div");
+      div4.id = "d4";
+        div4.style.backgroundColor = "#bf2232";
+          div4.style.width="939.43px";
+            div4.style.height="580.57px";
+
+var bd = document.getElementById("c3");
+      bd.appendChild(div4);
+
+var div5 = document.createElement("div");
+      div5.id = "e5";
+        div5.style.backgroundColor = "#950740";
+          div5.style.width="358.86px";
+            div5.style.height="580.57px";
+
+var bd = document.getElementById("d4");
+      bd.appendChild(div5);
+
+var div6 = document.createElement("div");
+      div6.id = "f6";
+        div6.style.backgroundColor = "#c3073f";
+          div6.style.width="358.86px";
+            div6.style.height="221.71px";
+
+var bd = document.getElementById("e5");
+     bd.appendChild(div6);
+
+var div7 = document.createElement("div");
+      div7.id = "g7";
+        div7.style.backgroundColor = "#e7717d";
+          div7.style.width="137.15px";
+            div7.style.height="221.71px";
+
+var bd = document.getElementById("f6");
+      bd.appendChild(div7);
+
+var div8 = document.createElement("div");
+      div8.id = "h8";
+        div7.style.backgroundColor = "#c2cad0";
+          div7.style.width="137.15";
+            div7.style.height="84.56px";
+
+var bd = document.getElementById("g7");
+      bd.appendChild(div8);
+
+var div9 = document.createElement("div");
+      div9.id = "i9";
+        div9.style.backgroundColor = "#7e585a";
+          div9.style.width="52.59";
+            div9.style.height="84.56px";
+
+var bd = document.getElementById("h8");
+      bd.appendChild(div9);
+      
